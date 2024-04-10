3b98f99dc0c1fe823b3c69991d505e2c38d0d8f9
pavel <pavelkoch7@yandex.ru>
2024-04-10 13:10:18 +0300

    ДЗ 8 готова

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,8 @@ public class Main {
         System.out.printf("\n\nЗадание 1\n");
         double amountOfSavings6 = 0;
         double monthlyContribution6 = 15000;
-        double percent6 = 1.01;
         int month = 0;
         while (amountOfSavings6 < 2_459_000) {
-            amountOfSavings6 *= percent6;
             amountOfSavings6 += monthlyContribution6;
             month++;
         }
@@ -44,7 +42,7 @@ public class Main {
         System.out.printf("\n\nЗадание 4\n");
         double amountOfSavings = 15000.00;
         double percent = 1.07;
-        for (int i = 0; amountOfSavings <= 12_000_000; i++) {
+        for (int i = 1; amountOfSavings <= 12_000_000; i++) {
             amountOfSavings *= percent;
             System.out.printf("Месяц %s, сумма накоплений составляет %.2f\n", i, amountOfSavings);
 
@@ -53,7 +51,7 @@ public class Main {
         // Задание 5
         System.out.printf("\n\nЗадание 5\n");
         amountOfSavings = 15000.00;
-        for (int i = 0; amountOfSavings <= 12_000_000; i++) {
+        for (int i = 1; amountOfSavings <= 12_000_000; i++) {
             amountOfSavings *= percent;
             if (i % 6 == 0) {
                 System.out.printf("Месяц %s, сумма накоплений составляет %.2f\n", i, amountOfSavings);
@@ -64,7 +62,7 @@ public class Main {
         System.out.printf("\n\nЗадание 6\n");
         amountOfSavings = 15000.00;
         int time = 9 * 12;
-        for (int i = 0; i <= time; i++) {
+        for (int i = 1; i <= time; i++) {
             amountOfSavings *= percent;
             if (i % 6 == 0) {
                 System.out.printf("Месяц %s, сумма накоплений составляет %.2f\n", i, amountOfSavings);
@@ -75,16 +73,16 @@ public class Main {
         System.out.printf("\n\nЗадание 7\n");
         System.out.printf("Введите дату первой пятницы месяца (1-7): ");
         int fridayDate = scanner.nextInt();
-        int daysInMonth =31;
+        int daysInMonth = 31;
         for (int i = fridayDate; i <= daysInMonth; i++) {
-            if ((i-fridayDate) % 7 == 0) {
+            if ((i - fridayDate) % 7 == 0) {
                 System.out.printf("Сегодня пятница, %s-е число. Необходимо подготовить отчет\n", i);
             }
         }
 
         // Задание 7 вариант 2
         System.out.printf("\n\nЗадание 7 вариант 2\n");
-        while (fridayDate<=31){
+        while (fridayDate <= 31) {
             System.out.printf("Сегодня пятница, %s-е число. Необходимо подготовить отчет\n", fridayDate);
             fridayDate += 7;
         }
@@ -95,7 +93,11 @@ public class Main {
         int thisYear = 2024;
         int startYear = thisYear - 200;
         int finishYear = thisYear + 100;
-        for (int i = startYear; i <= finishYear; i++){
+        for (int i = startYear; i <= finishYear; i++) {
+            if ((i < 0 || i > finishYear) && (i % 79 == 0)) {
+                System.out.printf("Год %s - за пределами нужного диапазона\n", i);
+                continue;
+            }
             if (i % 79 == 0) {
                 System.out.println(i);
             }
