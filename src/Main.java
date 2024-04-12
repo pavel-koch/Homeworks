ac338000ff4f54d6022c73f340dd1700864612a7
pavel <pavelkoch7@yandex.ru>
2024-04-12 22:59:16 +0300

    ДЗ 9 готово

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,51 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // Задание 1
+        System.out.printf("Задание 1\n");
+        int [] arrSum = {256, 712, 365, 514, 212};
+        // Вариант 1
+        System.out.printf("Сумма трат за месяц составила %s рублей\n", Arrays.stream(arrSum).sum());
+        // Вариант 2
+        double summa = 0;
+        for (int i : arrSum){
+            summa += i;
+        }
+        System.out.printf("Сумма трат за месяц составила %s рублей\n", summa);
 
-        int [] sum = {256, 712, 365, 514, 212};
-        System.out.println(Arrays.stream(sum).sum());
+        // Задание 2
+        System.out.printf("\n\nЗадание 2\n");
+        int minElem = arrSum[0];
+        int maxElem = arrSum[0];
+        // Вариант 1
+        for (int i : arrSum){
+            if (minElem > i) {
+                minElem = i;
+            }
+            if (maxElem < i) {
+                maxElem = i;
+            }
+        }
+        System.out.printf("Миннимальная сумма трат за неделю составила %s рублей.\n" +
+                "Максимальная сумма трат за неделю составила %s рублей\n", minElem, maxElem);
+
+        // Вариант 2
+        Arrays.sort(arrSum);
+        minElem = arrSum[0];
+        maxElem = arrSum[arrSum.length -1];
+        System.out.printf("Миннимальная сумма трат за неделю составила %s рублей.\n" +
+                "Максимальная сумма трат за неделю составила %s рублей\n", minElem, maxElem);
+
+        // Задание 3
+        System.out.printf("\n\nЗадание 3\n");
+        double averageExpenses = summa / arrSum.length;
+        System.out.printf("Средняя сумма трат за месяц составила %.2f рублей\n", averageExpenses);
+
+        // Задание 4
+        System.out.printf("\n\nЗадание 4\n");
+        char [] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.println(Arrays.toString(reverseFullName));
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
