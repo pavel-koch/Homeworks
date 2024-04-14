83c69e76b4fcc7e54f599a7037ccc55228b0dfb4
pavel <pavelkoch7@yandex.ru>
2024-04-14 12:26:01 +0300

    ДЗ 10

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.toString();
+        String middleName = scanner.toString();
+        String lastName = scanner.toString();
+        String fulName = lastName + firstName + middleName;
+
 
     }
 }
