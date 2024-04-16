aaebba2eb8c1e69a9659768c58437493c9a0de53
pavel <pavelkoch7@yandex.ru>
2024-04-16 22:23:23 +0300

    ДЗ 10 исправлено

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         // Задача 1
         System.out.println("Задача 1");
-        System.out.println("Введите ваше имя:");
-        String firstName = scanner.next();
-        System.out.println("Введите ваше отчество:");
-        String middleName = scanner.next();
-        System.out.println("Введите вашу Фамилию:");
-        String lastName = scanner.next();
-        String fulName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф.И.О сотрудника - " + fulName);
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О сотрудника - " + fullName);
 
         // Задача 2
         System.out.println("\nЗадача 2");
-        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета - " + fulName.toUpperCase());
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета - " + fullName.toUpperCase());
 
         // Задача 3
         System.out.println("\nЗадача 3");
-        fulName = fulName.replace("ё", "е");
-        System.out.println("Данные Ф.И.О сотрудника - " + fulName);
-
-
-
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О сотрудника - " + fullName);
     }
 }
